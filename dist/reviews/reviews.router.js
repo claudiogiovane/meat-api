66fba39421b599e98d5d61c00ee4bf716493e072
Claudio Giovane <claudiogiovane@gmail.com>
2018-05-29 16:14:12 -0300

    Chamando o populate de reviews com o usuário e o restaurante

--- dist/reviews/reviews.router.js
+++ dist/reviews/reviews.router.js
@@ -6,6 +6,17 @@ class ReviewsRouter extends model_router_1.ModelRouter {
     constructor() {
         super(reviews_model_1.Review);
     }
+    prepareOne(query) {
+        return query.populate('user', 'name')
+            .populate('restaurant', 'name');
+    }
+    /*findById = (req, resp, next)=>{
+        this.model.findById(req.params.id)
+            .populate('user', 'name')
+            .populate('restaurant', 'name')
+            .then(this.render(resp, next))
+            .catch(next)
+    }*/
     applyRoutes(application) {
         application.get('/reviews', this.findAll);
         application.get('/reviews/:id', [this.validateId, this.findById]);
--- reviews/reviews.router.ts
+++ reviews/reviews.router.ts
@@ -8,6 +8,19 @@ class ReviewsRouter extends ModelRouter<Review> {
         super(Review)
     }
 
+    protected prepareOne(query: mongoose.DocumentQuery<Review, Review>):mongoose.DocumentQuery<Review, Review>{
+        return query.populate('user', 'name')
+                    .populate('restaurant', 'name')
+      }
+
+    /*findById = (req, resp, next)=>{
+        this.model.findById(req.params.id)
+            .populate('user', 'name')
+            .populate('restaurant', 'name')
+            .then(this.render(resp, next))
+            .catch(next)
+    }*/
+
     applyRoutes(application: restify.Server) {
         application.get('/reviews', this.findAll)
         application.get('/reviews/:id', [this.validateId, this.findById])
